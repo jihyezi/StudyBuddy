51cedec79a9b1b2536b291883ed785f955b6a1bc
LeeJaeHo <l2281@naver.com>
2024-10-01 02:30:19 +0900

    feat : "Explore 검색 커뮤니티 컴포넌트 불러오기"

--- src/pages/Explore/SearchResulus.jsx
+++ src/pages/Explore/SearchResulus.jsx
@@ -1,22 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./SearchResulus.module.css";
 
 import JoinPostList from "components/Communities/CommunityJoinPostList";
-import { dummyPostData } from "components/Dummydata";
 import CommunityPostSmall from "components/Communities/CommunityPostSmall";
-
+import { fetchCommunities } from "../../components/Explore/communityData"; // 기본 내보내기
+import { dummyPostData } from "components/Dummydata";
 const SearchResults = () => {
   const location = useLocation();
   const query = new URLSearchParams(location.search).get("query");
 
   const [currentTab, clickTab] = useState(0);
+  const [communities, setCommunities] = useState([]);
+
+  useEffect(() => {
+    const getCommunities = async () => {
+      const data = await fetchCommunities(query);
+      console.log(data); // 데이터를 콘솔에 출력하여 확인
+      console.log(query); // 쿼리가 잘 가져와지는지 확인
+      setCommunities(data);
+    };
+
+    getCommunities();
+  }, [query]);
 
   const menuArr = [
     {
       name: "커뮤니티",
-      content: <JoinPostList postData={dummyPostData} />,
-      data: dummyPostData, // 데이터를 추가하여 비어있는지 확인 가능
+      content: (
+        <>
+          {communities.map((community) => (
+            <CommunityPostSmall
+              key={community.communityid}
+              communityimg={community.image}
+              communityname={community.name}
+              person={community.membercount} // 인원 수
+              post={community.postcount} // 게시글 수
+              date={new Date(community.createdat).toLocaleDateString()} // 시작일 포맷
+              communityicon="icon_url_here" // 아이콘 URL (여기에 실제 URL을 넣어주세요)
+            />
+          ))}
+        </>
+      ), // 검색 결과에 맞게 데이터 전달
+      data: communities, // 데이터를 추가하여 비어있는지 확인 가능
     },
     {
       name: "전체글",
@@ -59,7 +85,6 @@ const SearchResults = () => {
         ))}
       </div>
 
-      {/* 선택된 탭의 데이터가 존재하는지 확인하고 없으면 "검색 결과가 없습니다." 표시 */}
       {menuArr[currentTab].data && menuArr[currentTab].data.length > 0 ? (
         <div className={styles.desc}>{menuArr[currentTab].content}</div>
       ) : (
