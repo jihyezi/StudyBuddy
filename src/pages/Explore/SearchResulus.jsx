2d2e8b2308569ae55e5258fbd8240fab3edc5c05
LeeJaeHo <l2281@naver.com>
2024-10-05 04:21:25 +0900

    feat : "Explore 스터디 검색 기능 추가"

--- src/pages/Explore/SearchResulus.jsx
+++ src/pages/Explore/SearchResulus.jsx
@@ -1,115 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./SearchResulus.module.css";
 
 import JoinPostList from "components/Communities/CommunityJoinPostList";
 import CommunityPostSmall from "components/Communities/CommunityPostSmall";
-import { fetchCommunities } from "../../components/Explore/communityData";
-import { dummyPostData } from "components/Dummydata";
-import supabase from "components/supabaseClient";
-import { useAuth } from "contexts/AuthContext";
+import StudyPost from "components/Studies/StudyPost";
+
+import useCommunities from "../../components/Explore/hooks/useCommunities";
+import usePostsAndUsers from "../../components/Explore/hooks/usePostsAndUsers";
+import useStudies from "../../components/Explore/hooks/useStudies";
 
 const SearchResults = () => {
   const location = useLocation();
   const query = new URLSearchParams(location.search).get("query");
 
   const [currentTab, clickTab] = useState(0);
-  const [communities, setCommunities] = useState([]);
-  const [posts, setPosts] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [communityInfo, setCommunityInfo] = useState([]);
-  const [commentData, setCommentData] = useState([]);
-  const [allUser, setAllUser] = useState([]);
-  // const { user: sessionUser } = useAuth();
 
-  useEffect(() => {
-    const getCommunities = async () => {
-      const data = await fetchCommunities(query);
-      setCommunities(data);
-    };
-
-    getCommunities();
-  }, [query]);
-
-  useEffect(() => {
-    const fetchPostsUsersAndCommunities = async () => {
-      try {
-        const { data: postsData, error: postsError } = await supabase
-          .from("Post")
-          .select("*")
-          .ilike("title", `%${query}%`);
-
-        if (postsError) {
-          console.error("Error fetching posts:", postsError);
-          return;
-        }
-
-        if (!postsData || postsData.length === 0) {
-          console.log("No posts found for the given query.");
-          setPosts([]); // No posts, set to empty
-          return;
-        }
-
-        const userIds = postsData.map((post) => post.userid);
-        const { data: usersData, error: usersError } = await supabase
-          .from("User")
-          .select("*")
-          .in("userid", userIds);
-
-        if (usersError) {
-          console.error("Error fetching users:", usersError);
-          return;
-        }
-
-        const communityIds = postsData.map((post) => post.communityid);
-        const { data: communityData, error: communityError } = await supabase
-          .from("Community")
-          .select("*")
-          .in("communityid", communityIds);
-
-        if (communityError) {
-          console.error("Error fetching community data:", communityError);
-          return;
-        }
-
-        const allComments = await Promise.all(
-          postsData.map(async (post) => {
-            const { data: comments, error: commentError } = await supabase
-              .from("Comment")
-              .select("*")
-              .eq("postid", post.postid);
-            if (commentError) {
-              console.error("Error fetching comments:", commentError);
-              return [];
-            }
-            return comments;
-          })
-        );
-
-        setPosts(postsData);
-        setUsers(usersData);
-        setCommunityInfo(communityData);
-        setCommentData(allComments);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchPostsUsersAndCommunities();
-  }, [query]); // query가 변경될 때마다 데이터 fetch
-
-  useEffect(() => {
-    const fetchAllUserData = async () => {
-      const { data, error } = await supabase.from("User").select("*");
-
-      if (error) {
-        console.error("Error fetching all user data:", error);
-      } else {
-        setAllUser(data);
-      }
-    };
-    fetchAllUserData();
-  }, []);
+  const communities = useCommunities(query);
+  const { posts, users, communityInfo, commentData } = usePostsAndUsers(query);
+  const { studies, likesCount, commentsCount } = useStudies(query);
 
   const menuArr = [
     {
@@ -135,15 +44,33 @@ const SearchResults = () => {
           communityData={communityInfo}
           userData={users}
           comment={commentData}
-          allUserData={allUser}
         />
       ),
       data: posts,
     },
     {
       name: "스터디",
-      content: <JoinPostList postData={dummyPostData} />,
-      data: dummyPostData,
+      content:
+        studies.length > 0 ? (
+          studies.map((study) => (
+            <StudyPost
+              key={study.studyid}
+              studyId={study.studyid}
+              completion={study.completion}
+              title={study.title}
+              description={study.description.split("\n")[0]} // 첫 줄만 표시
+              tag={study.tag}
+              maxmembers={study.maxmembers}
+              proceed={study.proceed}
+              studyPost={study}
+              likesCount={likesCount[study.studyid] || 0}
+              commentsCount={commentsCount[study.studyid] || 0}
+            />
+          ))
+        ) : (
+          <div className={styles.Nosearchresults}>검색 결과가 없습니다.</div>
+        ),
+      data: studies,
     },
   ];
 
@@ -175,12 +102,9 @@ const SearchResults = () => {
           </div>
         ))}
       </div>
-
-      {menuArr[currentTab].data && menuArr[currentTab].data.length > 0 ? (
-        <div className={styles.desc}>{menuArr[currentTab].content}</div>
-      ) : (
-        <div className={styles.Nosearchresults}>검색 결과가 없습니다.</div>
-      )}
+      <div className={styles.communityPostSmallWrap}>
+        {menuArr[currentTab].content}
+      </div>
     </div>
   );
 };
