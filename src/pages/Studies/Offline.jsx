0e4a4b7874275895cb18876ec85b1dcb676cdf0f
jihyezi <154583345+jihyezi@users.noreply.github.com>
2024-10-04 18:24:48 +0900

    design: comingsoon 디자인 수정

--- src/pages/Studies/Offline.jsx
+++ src/pages/Studies/Offline.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import styles from './Offline.module.css'
 
 // images
-import commingsoon from "assets/images/commingsoon.gif"
+import comingsoon from "assets/images/comingsoon.gif"
 
 const Offline = () => {
   return (
     <div className={styles.OfflineContainer}>
-      <img src={commingsoon} className={styles.commingsoonicon} />
+      <img src={comingsoon} className={styles.comingsoonicon} />
     </div>
   );
 };
