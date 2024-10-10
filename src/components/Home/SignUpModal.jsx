e7ac42918a2274e7e4493d66201544e585edfa90
LeeJaeHo <l2281@naver.com>
2024-10-11 03:45:37 +0900

    feat : 회원가입 중복 체크 추가

--- src/components/Home/SignUpModal.jsx
+++ src/components/Home/SignUpModal.jsx
@@ -6,11 +6,9 @@ import close from "assets/icons/Home/close.png";
 import signUpimg from "assets/images/Home/signUpimg.png";
 import passwordicon from "assets/icons/Home/password.png";
 import rightarrow from "assets/icons/Home/rightarrow.png";
-import { createClient } from "@supabase/supabase-js";
-
-// const supabaseUrl = "https://vrpwhfbfzqwmqlhwhbtu.supabase.co";
-// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseKey);
+import red_error from "assets/icons/Home/red_error.svg";
+import blue_error from "assets/icons/Home/blue_error.svg";
+// import { createClient } from "@supabase/supabase-js";
 
 import supabase from "components/supabaseClient";
 
@@ -37,7 +35,14 @@ const SignUpModal = ({ modalIsOpen, closeModal }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  //nickname
   const [nickname, setNickname] = useState("");
+  const [nicknameMessage, setNicknameMessage] = useState("");
+  const [isNicknameAvailable, setIsNicknameAvailable] = useState(false);
+  // email
+  const [emailMessage, setEmailMessage] = useState("");
+  const [isEmailAvailable, setIsEmailAvailable] = useState(false);
+
   const [username, setUsername] = useState("");
   const [allChecked, setAllChecked] = useState(false);
   const [over14Checked, setOver14Checked] = useState(false);
@@ -45,11 +50,16 @@ const SignUpModal = ({ modalIsOpen, closeModal }) => {
   const [privacyChecked, setPrivacyChecked] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(""); // 에러 메시지 상태
+  // password 중복
+  const [confirmPasswordError, setConfirmPasswordError] = useState(""); // 비밀번호 확인 에러 상태
 
   const handleSignUp = async () => {
     if (password !== confirmPassword) {
+      setConfirmPasswordError("비밀번호가 일치하지 않습니다.");
       setError("비밀번호가 일치하지 않습니다.");
       return;
+    } else {
+      setConfirmPasswordError("");
     }
     if (!allChecked || !over14Checked || !termsChecked || !privacyChecked) {
       setError("모든 동의 항목을 확인해주세요.");
@@ -111,6 +121,90 @@ const SignUpModal = ({ modalIsOpen, closeModal }) => {
     setShowPassword(!showPassword);
   };
 
+  const checkNicknameDuplicate = async (nickname) => {
+    // 닉네임이 비어 있을 때는 중복 체크를 하지 않음
+    if (nickname.trim().length === 0) {
+      setNicknameMessage(""); // 메시지를 빈 값으로 설정
+      setIsNicknameAvailable(false);
+      return;
+    }
+
+    try {
+      const { data: nicknameData, error } = await supabase
+        .from("User")
+        .select("*")
+        .eq("nickname", nickname);
+
+      if (error) {
+        setNicknameMessage("닉네임 중복 확인 오류 발생");
+        setIsNicknameAvailable(false);
+        return;
+      }
+
+      if (nicknameData.length > 0) {
+        setNicknameMessage("이미 존재하는 닉네임입니다.");
+        setIsNicknameAvailable(false);
+      } else {
+        setNicknameMessage("사용 가능한 닉네임입니다.");
+        setIsNicknameAvailable(true);
+      }
+    } catch (error) {
+      setNicknameMessage("오류 발생: " + error.message);
+      setIsNicknameAvailable(false);
+    }
+  };
+
+  const handleNicknameChange = (e) => {
+    const value = e.target.value;
+    setNickname(value);
+
+    if (value.trim().length === 0) {
+      setNicknameMessage(""); // 닉네임이 없을 때 메시지를 빈 값으로 설정
+      return;
+    }
+
+    checkNicknameDuplicate(value); // 닉네임 중복 확인
+  };
+
+  const checkEmailDuplicate = async (email) => {
+    // 이메일이 비어 있을 때는 중복 체크를 하지 않음
+    if (!email.trim()) {
+      setEmailMessage(""); // 메시지를 빈 값으로 설정
+      setIsEmailAvailable(false);
+      return;
+    }
+
+    try {
+      const { data: emailData, error } = await supabase
+        .from("User")
+        .select("*")
+        .eq("email", email);
+
+      if (error) {
+        setEmailMessage("이메일 중복 확인 오류 발생");
+        setIsEmailAvailable(false);
+        return;
+      }
+
+      if (emailData.length > 0) {
+        setEmailMessage("이미 존재하는 계정입니다.");
+        setIsEmailAvailable(false);
+      } else {
+        setEmailMessage("사용 가능한 계정입니다.");
+        setIsEmailAvailable(true);
+      }
+    } catch (error) {
+      setEmailMessage("오류 발생: " + error.message);
+      setIsEmailAvailable(false);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+
+    checkEmailDuplicate(value); // 이메일 중복 확인
+  };
   return (
     <Modal
       isOpen={modalIsOpen}
@@ -132,16 +226,48 @@ const SignUpModal = ({ modalIsOpen, closeModal }) => {
             type="nickname"
             placeholder="닉네임"
             value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
+            onChange={handleNicknameChange}
             className={styles.inputField}
           />
+          {nicknameMessage && (
+            <div className={styles.errorContainer}>
+              <img
+                src={isNicknameAvailable ? blue_error : red_error}
+                alt={isNicknameAvailable ? "닉네임 사용 가능" : "닉네임 중복"}
+              />
+              <p
+                className={styles.nicknameMessage}
+                style={{
+                  color: isNicknameAvailable ? "#74A3FF" : "#FF7474",
+                }}
+              >
+                {nicknameMessage}
+              </p>
+            </div>
+          )}
           <input
             type="email"
             placeholder="이메일"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleEmailChange}
             className={styles.inputField}
           />
+          {emailMessage && (
+            <div className={styles.errorContainer}>
+              <img
+                src={isEmailAvailable ? blue_error : red_error}
+                alt={isEmailAvailable ? "이메일 사용 가능" : "이메일 중복"}
+              />
+              <p
+                className={styles.nicknameMessage}
+                style={{
+                  color: isEmailAvailable ? "#74A3FF" : "#FF7474",
+                }}
+              >
+                {emailMessage}
+              </p>
+            </div>
+          )}
           <input
             type="text"
             placeholder="사용자 이름"
@@ -179,8 +305,21 @@ const SignUpModal = ({ modalIsOpen, closeModal }) => {
               className={styles.passwordIcon}
             />
           </div>
-
-          {error && <div className={styles.error}>{error}</div>}
+          {confirmPasswordError && (
+            <div className={styles.errorContainer}>
+              <img
+                src={red_error} // 에러 아이콘은 항상 red_error 사용
+                alt="비밀번호가 일치하지 않습니다."
+              />
+              <p
+                className={styles.nicknameMessage}
+                style={{ color: "#FF7474" }}
+              >
+                {confirmPasswordError} {/* 비밀번호 불일치 메시지 */}
+              </p>
+            </div>
+          )}
+          {/* {error && <div className={styles.error}>{error}</div>} */}
 
           <div className={styles.checkboxContainer}>
             <div className={styles.checkboxItem}>
