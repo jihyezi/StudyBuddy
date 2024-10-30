2e865e0c89667b6286cc1250584ab6e4e30f6ca1
BlackShrike <qw991012@gmail.com>
2024-10-30 12:39:53 +0900

    리액트 쿼리 사용 및 채팅 삭제기능 추가

--- src/components/Messages/DeleteModal.jsx
+++ src/components/Messages/DeleteModal.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./DeleteModal.module.css";
 
-const DeleteModal = ({ onClose }) => {
+const DeleteModal = ({ onClose, onDelete }) => {
   return (
     <div className={styles.overlay} onClick={onClose}>
       <div className={styles.deleteModal} onClick={(e) => e.stopPropagation()}>
         <div className={styles.modalQuestion}>Delete Conversation?</div>
-        <div className={styles.modalConfirm} onClick={onClose}>
+        <div
+          className={styles.modalConfirm}
+          onClick={() => {
+            onDelete();
+            onClose();
+          }}
+        >
           Delete
         </div>
         <div className={styles.modalCancel} onClick={onClose}>
