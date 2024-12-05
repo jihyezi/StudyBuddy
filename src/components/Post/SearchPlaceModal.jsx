80621f2b852a84e0e059b783e490956151347973
jihyezi <154583345+jihyezi@users.noreply.github.com>
2024-12-05 22:56:36 +0900

    fix: api 수정

--- src/components/Post/SearchPlaceModal.jsx
+++ src/components/Post/SearchPlaceModal.jsx
@@ -24,18 +24,7 @@ const SearchPlaceModal = ({
   const handleSearch = async () => {
     if (searchTerm.length > 0) {
       try {
-        const client_id = process.env.NAVER_CLIENT_ID;
-        const client_secret = process.env.NAVER_CLIENT_SECRET;
-        const api_url = `/api/v1/search/local.json?query=${encodeURI(
-          searchTerm
-        )}&display=10`;
-        const options = {
-          headers: {
-            "X-Naver-Client-Id": client_id,
-            "X-Naver-Client-Secret": client_secret,
-          },
-        };
-        const response = await axios.get(api_url, options);
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/searchPlace?query=${encodeURIComponent(searchTerm)}`);
 
         // HTML 태그 제거 함수
         const removeHtmlTags = (str) => {
---
+++ src/pages/api/searchPlace.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    const { query } = req.query;  // 클라이언트에서 전달된 query 파라미터
+
+    const api_url = `https://openapi.naver.com/v1/search/local.json?query=${encodeURIComponent(query)}&display=10`;
+
+    try {
+        const response = await axios.get(api_url, {
+            headers: {
+                "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
+                "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET,
+            },
+        });
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error("Error fetching data from Naver API:", error);
+        res.status(500).json({ message: "Error fetching data", error: error.message });
+    }
+}
